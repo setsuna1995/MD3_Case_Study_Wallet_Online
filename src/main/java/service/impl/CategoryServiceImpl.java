c681693e7145bbe606f1d905d0136c57bb7fb347
setsuna1995 <133699203+setsuna1995@users.noreply.github.com>
2023-10-05 10:20:47 +0700

    update

--- src/main/java/service/impl/CategoryServiceImpl.java
+++ src/main/java/service/impl/CategoryServiceImpl.java
@@ -1,9 +1,15 @@
 package service.impl;
 
 import model.Category;
+import model.User;
 import service.CategoryService;
+import util.ConnectionUtil;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CategoryServiceImpl implements CategoryService {
@@ -19,7 +25,25 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public List<Category> findAll() {
-        return null;
+        List<User> users = new ArrayList<>();
+        try (Connection connection = new ConnectionUtil();
+             PreparedStatement preparedStatement = connection.prepareStatement("select * from product");) {
+            System.out.println(preparedStatement);
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String userName = rs.getString("userName");
+                String password = rs.getString("password");
+                String address = rs.getString("address");
+                float totalMoney = Float.parseFloat(rs.getString("totalMoney"));
+                users.add(new User(id, name, userName, password, address, totalMoney));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return users;
     }
 
     @Override
